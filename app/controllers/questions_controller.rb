d320568a4c4ce86531d4034d65700466ad006773
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-08-31 11:02:05 +0900

    set strong-parameter

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,6 +16,7 @@ class QuestionsController < ApplicationController
       redirect_to root_path, notice: 'Success!'
     else
       flash[:alert] = 'Save error!'
+      render :new
     end
   end
 
